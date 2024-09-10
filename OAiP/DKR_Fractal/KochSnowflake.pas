d6041b660c8a186efb67765d04f58f51899670c9
Fynjy28 <101527317+Fynjy28@users.noreply.github.com>
2024-09-10 09:50:11 +0300

    Add files via upload

---
+++ OAiP/DKR_Fractal/KochSnowflake.pas
@@ -0,0 +1,34 @@
+﻿uses
+  GraphABC, KochSnowflakeUnit;
+
+var
+  x1, y1, x2, y2, x3, y3, size: Real;
+  depth: Integer;
+ 
+procedure KeyDown(Key: Integer);
+begin
+  case Key of
+    VK_Left: x1 := x1 - 10;
+    VK_Right: x1 := x1 + 10;
+    VK_Up: y1 := y1 - 10;
+    VK_Down: y1 := y1 + 10;
+    VK_O: if depth < 5 then depth += 1;
+    VK_P: if depth >= 0 then depth -= 1;
+    189: if size > 10 then size := size - 10;
+    187: size := size + 10;
+  end;
+  UpdateCoords(x1, y1, size, x2, y2, x3, y3);
+  DrawSnowflake(x1, y1, x2, y2, x3, y3, size, depth);
+end;
+
+begin
+  SetWindowSize(500,500);
+  SetWindowCaption('Снежинка Коха(или треугольник Коха)');
+  size := 400;
+  x1 := 100;
+  y1 := 354;
+  depth := 1; 
+  UpdateCoords(x1, y1, size, x2, y2, x3, y3);
+  DrawSnowflake(x1, y1, x2, y2, x3, y3, size, depth);
+  OnKeyDown := KeyDown;
+end.
---
+++ OAiP/DKR_Fractal/KochSnowflakeUnit.pas
@@ -0,0 +1,66 @@
+﻿unit KochSnowflakeUnit;
+
+interface
+
+uses GraphABC;
+
+procedure DrawSnowflake(x1, y1, x2, y2, x3, y3, size: Real; depth: Integer);
+procedure UpdateCoords(var x1, y1: Real; size: Real; var x2, y2, x3, y3: Real);
+
+implementation
+
+procedure Draw(x, y, length, grad: Real; t: Integer); forward;
+procedure Draw2(var x, y: Real; length, grad: Real; depth: Integer); forward;
+
+procedure Draw(x, y, length, grad: Real; t: Integer);
+begin
+  if t > 0 then
+  begin
+    length := length / 3;
+    Draw2(x, y, length, grad, t-1);
+    Draw2(x, y, length, grad + Pi / 3, t-1);
+    Draw2(x, y, length, grad - Pi / 3, t-1);
+    Draw2(x, y, length, grad, t-1);
+  end
+  else
+    Line(Round(x), Round(y), Round(x + Cos(grad) * length), Round(y - Sin(grad) * length));
+end;
+
+procedure Draw2(var x, y: Real; length, grad: Real; depth: Integer);
+begin
+  Draw(x, y, length, grad, depth);
+  x := x + length * Cos(grad);
+  y := y - length * Sin(grad);
+end;
+
+procedure UpdateCoords(var x1, y1: Real; size: Real; var x2, y2, x3, y3: Real);
+var
+  katet: Real;
+begin
+  x2 := x1 + size;
+  katet := size / 2;
+  x3 := x1 + katet;
+  y2 := y1;
+  y3 := y1 - katet * Power(3, 1/2);
+end;
+
+procedure Words;
+begin
+  textout(0, 0, 'Стрелки - передвижение фрактала');
+  textout(0, 17, '+/- - изменение масштаба');
+  textout(0, 34, 'O/P - изменение глубины фрактала');
+end;
+
+procedure DrawSnowflake(x1, y1, x2, y2, x3, y3, size: Real; depth: Integer);
+begin
+  LockDrawing;
+  ClearWindow;
+  Draw(x1, y1, size, pi/3, depth);
+  Draw(x2, y2, size, pi, depth);
+  Draw(x3, y3, size, -pi/3, depth);
+  Words;
+  Redraw;
+end;
+
+end.
+
